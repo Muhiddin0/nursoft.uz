ebe43f9aef79c0284099f71ad5a21c8de6c0ac1a
muhiddin0 <kabralievmuhiddinjon02@gmail.com>
2023-09-19 08:44:35 +0500

    first commit

---
+++ pages/components.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 style="text-align: center;">I am Component</h1>
+
+    <button :style="`${button_sm}`" class="border-2 text-red-400">Button</button>
+
+</template>
+
+<script>
+
+import { button_sm } from '~/theme';
+
+export default {
+    setup () {
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <Head>
+        <link rel="stylesheet" href="../static/styles/main.css">
+    </Head>
+    
+    <h1>HI I'm live</h1>
+
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
